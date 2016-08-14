e514da620c5ba036d569309b98f3e4d38ef1ce16
Iain Dooley <iain@workingsoftware.com.au>
2016-08-14 12:07:44 +1000

    Revert "Added in mods from Alex"
    This reverts commit d6199d93ece4f2dfce4504196bb55ef898bd0c20.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-var togglers = document.querySelectorAll('.offcanvas--toggler');
-
-for (var i = 0; i < togglers.length; i++) {
-	togglers[i].addEventListener('click', function() {
-		document.body.classList.toggle('offcanvas--show')
+$(document).ready(function() {
+	$('.offcanvas--toggler').on('click', function(e) {
+		console.log('test');
+		$('body').toggleClass('offcanvas--show');
 	});
-}
+});
